08607070f72fb0b0a5785e46fefe75a62972b3a5
shantialouis <shantialouis@gmail.com>
2021-08-22 00:05:37 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './component/MovieList';
+import MovieListHeading from './component/MovieListHeading';
+import SearchBox from './component/SearchBox';
+import AddFavourite from './component/AddFavourite';
+
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+	const [favourite, setFavourite] = useState([]);
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourite, movie];
+		setFavourite(newFavouriteList);
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					favouriteComponent={AddFavourite}
+					handleFavouritesClick={addFavouriteMovie}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList movies={favourite} favouriteComponent={AddFavourite} />
+			</div>
+		</div>
+	);
+};
+
+export default App;
